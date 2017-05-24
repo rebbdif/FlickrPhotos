b3bb39ec403d6c40b88dd5afc02ad3fff8108afc
Leonid Serebryanyy <leosilver@yandex.ru>
2017-05-24 23:26:50 +0300

    minimal task done

---
+++ lection14/Services/SLVImageProcessing.m
@@ -0,0 +1,13 @@
+//
+//  SLVImageProcessing.m
+//  lection14
+//
+//  Created by 1 on 24.05.17.
+//  Copyright © 2017 iOS-School-1. All rights reserved.
+//
+
+#import "SLVImageProcessing.h"
+
+@implementation SLVImageProcessing
+
+@end
